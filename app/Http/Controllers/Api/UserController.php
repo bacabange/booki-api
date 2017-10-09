38679340961ffa1d884bfe409ef9fc7f4585760d
Stiven Castillo <stivencastillo.90@gmail.com>
2017-10-08 23:36:19 -0500

    List user books endpoint paginate

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BookCollection;
 
 class UserController extends Controller
 {
     public function listBooks()
     {
+        $user = \Auth::user();
+
+        return new BookCollection($user->books()->paginate());
     }
 }
